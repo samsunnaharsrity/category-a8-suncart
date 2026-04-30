994d0a4f16e0fe343449304da408dae7b78dae17
Samsunnahar Srity <samsunnaharsrity@gmail.com>
2026-05-01 02:21:53 +0600

    customized json file data

--- src/components/productCart/productCart.jsx
+++ src/components/productCart/productCart.jsx
@@ -10,9 +10,31 @@ console.log(latestProduct);
 
             <Card>
                 <div>
-                  <img src={latestProduct.image} alt={latestProduct.name} />
+                  <img src={latestProduct.image} 
+                  alt={latestProduct.name} 
+                  className=''/>
                 </div>
-                <div></div>
+                <div>
+                    <h2 className='font-semibold'>{latestProduct.name}</h2>
+                </div>
+
+                <div>
+                    <p>{latestProduct.rating}</p>
+                </div>
+
+<div className="flex items-center gap-2">
+  <span className="text-xl font-bold text-orange-500">
+    ${latestProduct.discountPrice}
+  </span>
+
+  <span className="text-gray-400 line-through">
+    ${latestProduct.price}
+  </span>
+
+  <span className="bg-red-500 text-white text-xs px-2 py-1 rounded">
+    50% OFF
+  </span>
+</div>
             </Card>
 
     );
