bea7f9d701c53bbeaad1a8c8bc9e15f123af6812
Samsunnahar Srity <samsunnaharsrity@gmail.com>
2026-05-02 14:18:47 +0600

    proxy file added and design

---
+++ src/proxy.js
@@ -0,0 +1,24 @@
+import { NextResponse } from 'next/server'
+import { auth } from './lib/auth'
+import { headers } from 'next/headers'
+ 
+// This function can be marked `async` if using `await` inside
+export async function proxy(request) {
+
+//   return NextResponse.redirect(new URL('/home', request.url))
+// console.log("proxy message");
+
+
+const session = await auth.api.getSession({
+    headers: await headers() // you need to pass the headers object.
+})
+
+if (!session){
+     return NextResponse.redirect(new URL('/signIn', request.url))
+}
+
+}
+ 
+export const config = {
+  matcher: ['/myProfile' , '/all-products/:path'],
+}
